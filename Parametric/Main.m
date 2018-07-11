c20ee28657634ab23e3628cb2806eb5f86560ca9
Ziyue Wang <zwang@icr.ac.uk>
2018-07-11 16:39:09 +0100

    quicker now taken out OOp

--- Parametric/Main.m
+++ Parametric/Main.m
@@ -1,8 +1,10 @@
 %%Main
 
 %% User Data 
+%tic
 %file = uigetfile;
 %load(file);
+%toc
 
 radius = str2double(inputdlg('Enter radius of ROI'));
 
@@ -11,55 +13,66 @@ radius = str2double(inputdlg('Enter radius of ROI'));
 yLength = length(ImgData{2,1}(1,:,1,1));        %Finds the length of the image. Basically the x and the y region
 xLength = length(ImgData{2,1}(:,1,1,1));        %of the image. 
 
-PixelMap(xLength,yLength) = PixelVector(1,1,2);
+%PixelMap(xLength,yLength) = PixelVector(1,1,2);
+PixelMap = zeros(xLength,yLength,850);
 %pixel = [1:xLength*yLength];
 
-for x = 1:xLength
-    for y = 1:yLength
+tic
+for y = 1:yLength
+    for x = 1:xLength
         intensity = [];
-        for i = 1:ImgLastFrame_PI
-            intensity(i) = ImgData{2,1}(x,y,1,i);
-        end
-        %PixelMap(x,y) = PixelVector(x,y,intensity);
-        PixelMap(x,y).xCoord = x;
-        PixelMap(x,y).yCoord = y;
-        PixelMap(x,y).intensity = intensity;
+        %for i = 2:849
+        %    intensity(i) = ImgData{2,1}(x,y,1,i);
+        %end
+        
+        %PixelMap(x,y).xCoord = x;
+        %PixelMap(x,y).yCoord = y;
+        %PixelMap(x,y).intensity = intensity;
+        
+        PixelMap(x,y,:) = ImgData{2,1}(x,y,1,:);
     end
 end
-
+toc
 %% Calculating TIC for each pixel ROI
 
-TICMap(xLength,yLength) = PixelVector(1,1,1);
+%TICMap(xLength,yLength) = PixelVector(1,1,1);
+TICMap = zeros(xLength,yLength,850);
 
-for x = 1:xLength
-    for y = 1:yLength
-        IntensitySum = PixelMap(x,y).intensity;
+tic
+parfor y = 1:yLength
+    for x = 1:xLength
+        IntensitySum = PixelMap(x,y,:);
         totalArea = (2*radius+1)^2;          %1 per pixel
+        if radius > 0
         dgLeft = 0;dgUp = 0; dgRight = 0; dgDown = 0;
         leftLimit = radius; rightLimit = radius; upLimit = radius; downLimit = radius;
         
         
         %Up right down left first. Checks if they're available
         if x-radius >= 1
-            IntensitySum = IntensitySum + PixelMap(x-radius,y).intensity;
+            %IntensitySum = IntensitySum + PixelMap(x-radius,y).intensity;
+            IntensitySum = IntensitySum + PixelMap(x-radius,y,:);
         else
             dgLeft = 1;
             leftLimit = x-1;
         end
         if x+radius <= xLength
-            IntensitySum = IntensitySum + PixelMap(x+radius,y).intensity;
+            %IntensitySum = IntensitySum + PixelMap(x+radius,y).intensity;
+            IntensitySum = IntensitySum + PixelMap(x+radius,y,:);
         else
             dgRight = 1;
             rightLimit = xLength-x;
         end
         if y-radius >= 1
-            IntensitySum = IntensitySum + PixelMap(x,y-radius).intensity;
+            %IntensitySum = IntensitySum + PixelMap(x,y-radius).intensity;
+            IntensitySum = IntensitySum + PixelMap(x,y-radius,:);
         else
             dgUp = 1;
             upLimit = y-1;
         end
         if y+radius <= yLength
-            IntensitySum = IntensitySum + PixelMap(x,y+radius).intensity;
+            %IntensitySum = IntensitySum + PixelMap(x,y+radius).intensity;
+            IntensitySum = IntensitySum + PixelMap(x,y+radius,:);
         else
             dgDown = 1;
             downLimit = yLength-y;
@@ -70,42 +83,49 @@ for x = 1:xLength
         for u = 1:leftLimit
             for v = 1:upLimit
                 %Left and up    
-                IntensitySum = IntensitySum + PixelMap(x-u,y-v).intensity;
+                %IntensitySum = IntensitySum + PixelMap(x-u,y-v).intensity;
+                IntensitySum = IntensitySum + PixelMap(x-u,y-v,:);
             end
         end
         
         for u = 1:rightLimit
             for v = 1:upLimit
                 %Left and up    
-                IntensitySum = IntensitySum + PixelMap(x+u,y-v).intensity;
+                %IntensitySum = IntensitySum + PixelMap(x+u,y-v).intensity;
+                IntensitySum = IntensitySum + PixelMap(x+u,y-v,:);
             end
         end
         
         for u = 1:leftLimit
             for v = 1:downLimit
                 %Left and up    
-                IntensitySum = IntensitySum + PixelMap(x-u,y+v).intensity;
+                %IntensitySum = IntensitySum + PixelMap(x-u,y+v).intensity;
+                IntensitySum = IntensitySum + PixelMap(x-u,y+v,:);
             end
         end
         
         for u = 1:rightLimit
             for v = 1:downLimit
                 %Left and up    
-                IntensitySum = IntensitySum + PixelMap(x+u,y+v).intensity;
+                %IntensitySum = IntensitySum + PixelMap(x+u,y+v).intensity;
+                IntensitySum = IntensitySum + PixelMap(x+u,y+v,:);
             end
         end
+        end
         
-        TICMap(x,y).xCoord = x;
-        TICMap(x,y).yCoord = y;
-        TICMap(x,y).intensity = IntensitySum/totalArea;
+        %TICMap(x,y).xCoord = x;
+        %TICMap(x,y).yCoord = y;
+        %TICMap(x,y).intensity = IntensitySum/totalArea;
+        
+        TICMap(x,y,:) = IntensitySum/totalArea;
     end
 end
-
+toc
 
 %% Calculates WIT and PI for each small pixel
 
 
-%gradientIntensity = gradient(TICMap(5,5).intensity);        %Find's the gradient at each point of the curve
+%gradientIntensity = gradient(TICMap(50,100).intensity);        %Find's the gradient at each point of the curve
 
 
 %threshold = 20;             %This should be done by inspection analysis. Assumed to be 20 for my results so far.
@@ -119,10 +139,14 @@ end
 PIMap = zeros(xLength,yLength);
 WITMap = zeros(xLength,yLength);
 
+tic 
 for x = 1:xLength
     for y = 1:yLength
-          findMax = max(TICMap(x,y).intensity);
-          index = find(TICMap(x,y).intensity == findMax);
+          %findMax = max(TICMap(x,y).intensity);
+          findMax = max(TICMap(x,y,:));
+          if findMax > 0
+          %index = find(TICMap(x,y).intensity == findMax);
+          index = find(TICMap(x,y,:) == findMax);
           timeStamp = (index-1)/10;     %Matlab index starts at 1, so 0.0 second is actually 1st frame. 
           
           
@@ -134,8 +158,11 @@ for x = 1:xLength
           WITMap(x,y) = timeStamp;
           PIMap(x,y) = findMax;
           %end
+          end
     end
 end
+
+toc
 
 %% does the parametric plotting
 figure
@@ -163,3 +190,4 @@ view(2)
 set(gca,'XAxisLocation','top','YAxisLocation','left','ydir','reverse');
 colormap hot
 colorbar
+
