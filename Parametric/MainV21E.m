ed6ac7d0ccd941dbec83db3b7ff4d91b190e950d
ZiyueWangUoB <33383959+ZiyueWangUoB@users.noreply.github.com>
2018-07-12 21:31:43 +0100

    need work on small black regions

--- Parametric/MainV21E.m
+++ Parametric/MainV21E.m
@@ -33,7 +33,7 @@ toc
 TICMap = zeros(xLength+2*radius,yLength+2*radius,maxFrames);
 
 tic
-for y = 1+radius:yLength+radius
+parfor y = 1+radius:yLength+radius
     for x = 1+radius:xLength+radius
         %IntensitySum = PixelMap(x,y,:);
         %totalArea = (2*radius+1)^2;          %1 per pixel
@@ -82,17 +82,36 @@ for x = 1:xLength
           timeStamp = (index-1)/10;     %Matlab index starts at 1, so 0.0 second is actually 1st frame.
           
           
-          r = reshape(TICMap(100+radius,100+radius,:),[1,maxFrames]);
+          r = reshape(TICMap(x+radius,y+radius,:),[1,maxFrames]);
           gk = diff(r,2);
+          gk(end) = [];
+          maxGrad = max(gk(:));
           
-          maxNoise = max(gk(1:80));
-          ATIndex = find(gk > maxNoise,1);          % Index - 1 would be the time but im lazy sorry
+                 
+%           
+          maxNoise = max(gk(1:150));
+          ATIndexYeah = 0;
+          if maxNoise < maxGrad 
+ 0;          % Index - 1 would be the time but im lazy sorry
+         for j = 1:length(ATIndex)
+          if ATIndex(j) < maxFrames-65
+          AvgAfterIndex = mean(abs(gk(ATIndex(j):ATIndex(j)+50)));
+                   
+          if ((ATIndex(j) > 10) && (AvgAfterIndex > gk(ATIndex(j))))
+              ATIndexYeah = ATIndex(j);
+              break;
+          else
+              ATIndexYeah = 0;
+          end
+          end
+          end
+         end
           
           MTTEndIntensity = 0.65*findMax;
-          MTTEndIndex = find(r > MTTEndIntensity,1,'last');
+          MTTEndIndex = find(r >= MTTEndIntensity,1,'last');
           
-          ATMap(x,y) = (ATIndex-1)/10;
-          MTTMap = (MTTEndIndex-ATIndex)/10;
+          ATMap(x,y) = (ATIndexYeah-1)/10;
+          MTTMap(x,y) = (MTTEndIndex-ATIndexYeah)/10;
           WITMap(x,y) = timeStamp;
           PIMap(x,y) = findMax;
           end
@@ -103,7 +122,7 @@ toc
 
 %% does the parametric plotting
 figure
-subplot(2,2,1)
+subplot(1,2,1)
 surf(PIMap)
 title(['PI parametric radius ' num2str(radius)])
 %xlabel('X')
@@ -116,7 +135,7 @@ colormap hot
 colorbar
 hold on
 
-subplot(2,2,2)
+subplot(1,2,2)
 surf(WITMap)
 title(['WIT parametric radius ' num2str(radius)])
 %xlabel('X')
@@ -128,7 +147,9 @@ set(gca,'XAxisLocation','top','YAxisLocation','left','ydir','reverse');
 colormap hot
 colorbar
 
-subplot(2,2,3)
+figure
+
+subplot(1,2,1)
 surf(ATMap)
 title(['AT parametric radius ' num2str(radius)])
 %xlabel('X')
@@ -140,7 +161,7 @@ set(gca,'XAxisLocation','top','YAxisLocation','left','ydir','reverse');
 colormap hot
 colorbar
 
-subplot(2,2,4)
+subplot(1,2,2)
 surf(MTTMap)
 title(['MTT parametric radius ' num2str(radius)])
 %xlabel('X')
@@ -153,8 +174,16 @@ colormap hot
 colorbar
 
 %%
+          r = reshape(TICMap(x+radius,y+radius,:),[1,maxFrames]);
+          gk = diff(r,2);
+          gk(end) = [];
+          maxGrad = max(gk(:));
 
-
+          figure
+          plot(1:maxFrames,r)
+          
+          figure
+          plot(1:maxFrames-3,gk)  
 
 
 
